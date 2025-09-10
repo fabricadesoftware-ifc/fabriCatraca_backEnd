9917fc8c35b1d6c2ede783c30fa7e9224ddf955b
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-09-10 13:20:11 -0300

    Adiciona campo 'users' ao CustomGroupSerializer para incluir usuários associados ao grupo, melhorando a representação dos dados. Implementa método para recuperar informações dos usuários relacionados.

--- src/core/control_Id/infra/control_id_django_app/serializers/group.py
+++ src/core/control_Id/infra/control_id_django_app/serializers/group.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from src.core.control_Id.infra.control_id_django_app.models import CustomGroup
+from src.core.user.infra.user_django_app.models import User
 
 class CustomGroupSerializer(serializers.ModelSerializer):
+    users = serializers.SerializerMethodField()
+
     class Meta:
         model = CustomGroup
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'users']
         read_only_fields = ['id']
+
+    def get_users(self, obj):
+        return [{'id': user.id, 'name': user.name} for user in User.objects.filter(usergroup__group=obj)]
