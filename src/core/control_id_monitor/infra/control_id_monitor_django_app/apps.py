a012363921ee2e74f388681d7f99d21fc72bd76d
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-10-01 13:07:15 -0300

    feat: adiciona arquivos iniciais para o aplicativo Django de controle de ID

---
+++ src/core/control_id_monitor/infra/control_id_monitor_django_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MonitorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "src.core.control_id_monitor.infra.control_id_monitor_django_app"
