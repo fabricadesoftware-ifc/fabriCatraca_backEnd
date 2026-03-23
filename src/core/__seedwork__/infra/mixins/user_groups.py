1a6d84897c5003b7c1ce7be70457cbdbda41d2e3
jonatasperaza <jonatassilvaperaza@gmail.com>
2026-03-23 20:03:33 -0300

    feat: remover método de sincronização de grupos de usuários e dependências não utilizadas

--- src/core/__seedwork__/infra/mixins/user_groups.py
+++ src/core/__seedwork__/infra/mixins/user_groups.py
@@ -1,7 +1,5 @@
 from src.core.__seedwork__.infra import ControlIDSyncMixin
-from django.db import transaction
 from rest_framework.response import Response
-from rest_framework import status
 
 from src.core.__seedwork__.infra.mixins._typing import UserGroupLike
 from src.core.__seedwork__.infra.types.user_groups import UserGroupsData
@@ -42,25 +40,3 @@ class UserGroupsSyncMixin(ControlIDSyncMixin):
             },
         )
         return response
-
-    def sync_from_catraca(self) -> Response:
-        try:
-            from src.core.control_Id.infra.control_id_django_app.models import UserGroup
-
-            catraca_objects = self.load_objects("user_groups")
-
-            with transaction.atomic():
-                UserGroup.objects.all().delete()
-                for data in catraca_objects:
-                    UserGroup.objects.create(**data)
-
-            return Response(
-                {
-                    "success": True,
-                    "message": f"Sincronizados {len(catraca_objects)} usuários em grupos",
-                }
-            )
-        except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
