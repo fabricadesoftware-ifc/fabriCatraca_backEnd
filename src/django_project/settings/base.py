f78e6702516fcc5f4dd03e10c8c4a7327aad96ea
jonatasperaza <jonatassilvaperaza@gmail.com>
2026-05-14 11:23:06 -0300

    feat: adicionar tempo de expiração para tokens de atualização no Simple JWT

--- src/django_project/settings/base.py
+++ src/django_project/settings/base.py
@@ -132,6 +132,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
 SPECTACULAR_SETTINGS = {
